8a987986a4b1fe822f0cec469107f96a8783589b
aczerbe <61129007+aczerbe@users.noreply.github.com>
2023-04-29 19:50:42 -0700

    Basic functionality on simple problems

---
+++ project2_py/plotter.py
@@ -0,0 +1,65 @@
+from tqdm import tqdm
+import numpy as np
+import project2
+import helpers
+import matplotlib.pyplot as plt
+
+def main():
+	p = helpers.Simple1()
+	(x_hist_1, f_hist_1) = run_optimizer(project2.cross_entropy_method, p, np.array([0, 0]))
+	#p = helpers.Simple2()
+	#(x_hist_2, f_hist_2) = run_optimizer(project1.gradient_descent, p, [-2, 1.7])
+	#p = helpers.Simple3()
+	#(x_hist_3, f_hist_3) = run_optimizer(project1.gradient_descent, p, [1,1,1,1])
+
+
+	fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+	ax2.plot(f_hist_1)
+	ax2.set_title('Simple1 Convergence Plot')
+	ax2.set_xlabel('Iterations')
+	ax2.set_ylabel('Value')
+
+	# ax3.plot(f_hist_2)
+	# ax3.set_title('Simple2 Convergence Plot')
+	# ax3.set_xlabel('Iterations')
+	# ax3.set_ylabel('Value')
+
+	# ax4.plot(f_hist_3)
+	# ax4.set_title('Simple3 Convergence Plot')
+	# ax4.set_xlabel('Iterations')
+	# ax4.set_ylabel('Value')
+
+	
+	contourSize = [50,50]
+	X = np.linspace(-3,3,contourSize[0])
+	Y = np.linspace(-3,3,contourSize[1])
+	Z = np.zeros(contourSize)
+	p = helpers.Simple1()
+	for i in range(len(X)):
+		for j in range(len(Y)):
+			Z[j][i] = p.f(np.array([X[i],Y[j]]))
+	ax1.contour(X,Y,Z, 100)
+	x_hist_1 = np.array(x_hist_1)
+	ax1.plot(x_hist_1[:,0],x_hist_1[:,1])
+	p = helpers.Simple1()
+	(x_hist_1b, f_hist_1b) = run_optimizer(project2.cross_entropy_method, p, np.array([1, 1]))
+	p = helpers.Simple1()
+	(x_hist_1c, f_hist_1c) = run_optimizer(project2.cross_entropy_method, p, np.array([0.5, 0.5]))
+	x_hist_1b = np.array(x_hist_1b)
+	ax1.plot(x_hist_1b[:,0],x_hist_1b[:,1])
+	x_hist_1c = np.array(x_hist_1c)
+	ax1.plot(x_hist_1c[:,0],x_hist_1c[:,1])
+	ax1.set_title('Simple1 Contour Plot')
+
+	# plt.tight_layout()
+
+	plt.show()
+
+
+def run_optimizer(optimizer, p, x0):
+    x_hist = optimizer(p.f, p.g, p.c, x0, p.n, p.count, p.prob)
+    f_hist = [p.f(j) for j in x_hist]
+    return (x_hist, f_hist)
+
+if __name__ == '__main__':
+	main()
--- project2_py/project2.py
+++ project2_py/project2.py
@@ -14,6 +14,7 @@ Note: Do not import any other modules here.
 '''
 import numpy as np
 
+params_table = {'simple1': [100, 5], 'simple2': [100, 10], 'simple3': [100, 10], 'secret1': [100, 10], 'secret2': [100, 10]}
 
 def optimize(f, g, c, x0, n, count, prob):
     """
@@ -31,5 +32,56 @@ def optimize(f, g, c, x0, n, count, prob):
         x_best (np.array): best selection of variables found
     """
     x_best = x0
-
+    #if prob == 'simple1':
+    x_hist = cross_entropy_method(f, g, c, x0, n, count, prob)
+    x_best = x_hist[-1]
     return x_best
+
+def cross_entropy_method(f,g,c, x0, n, count, prob):
+    x_hist = [x0]
+    m = params_table[prob][0]
+    m_elite = params_table[prob][1]
+    rng = np.random.default_rng()
+    cov = np.eye(len(x0))
+    mean = x0
+    #print('original: ' + str(x0))
+    #print("constraint on original: " + str(c(x0)))
+
+    while count() < n - (m*2):
+        samples = rng.multivariate_normal(mean, cov, m)
+        
+        samples_pruned = []
+        for j in samples:
+            if in_bounds(j, c):
+                samples_pruned.append(j)
+                #print(j)
+
+        #print()
+        evals = [f(j) for j in samples_pruned]
+        elite_samples = [samples_pruned[i] for i in np.argsort(evals)[0:m_elite]]
+        #print(elite_samples)
+        if(len(elite_samples) < 2):
+            cov = cov * 1.5
+        else:
+            mean = np.mean(elite_samples, axis=0)
+            cov = np.cov(elite_samples, rowvar=0)
+            x_hist.append(mean)
+        #print(mean)
+    return x_hist
+
+
+
+def gradient_descent(f,g,x0,n,count, prob):
+    alpha = alpha_table[prob]
+    x_hist = [x0]
+    while count() < n:
+        x_hist.append(x_hist[-1] - alpha * g(x_hist[-1]))
+    return x_hist
+
+def constrained_f_inv(f, c, rho, x):
+    return f(x) + -1/(rho * c(x))
+
+def in_bounds(x, c):
+    a = c(x)
+    #print(str(x) + ": " + str(a))
+    return not any(t > 0 for t in a)
